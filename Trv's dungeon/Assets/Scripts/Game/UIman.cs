67a7fa0d33bb387dceb189324cf48e8fa981a9f3
wkis028 <wkis028@live.rhul.ac.uk>
2024-12-01 16:29:13 +0000

    Added time tracker as score

--- Trv's dungeon/Assets/Scripts/Game/UIman.cs
+++ Trv's dungeon/Assets/Scripts/Game/UIman.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class UIman : MonoBehaviour
 {
+
+    public float timeValue = 0;
+    private bool _gameStop = false;
+    public TextMeshProUGUI TimeText;
+    public TextMeshProUGUI finalScore;
     [SerializeField] private Image[] _liveImages;
     [SerializeField] private TextMeshProUGUI _gameoverText;
     [SerializeField] private TextMeshProUGUI _gamewinText;
@@ -15,10 +20,32 @@ public class UIman : MonoBehaviour
     }
     public void GameOverSequence()
     {
+        _gameStop = true;
         _gameoverText.gameObject.SetActive(true);
     }
     public void GameWinSequence()
     {
+        _gameStop = true;
         _gamewinText.gameObject.SetActive(true);
+        finalScore.gameObject.SetActive(true);
+        TimeText.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        if (_gameStop == false)
+        {
+            timeValue += Time.deltaTime;
+        }
+        DisplayTime(timeValue);
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        float Minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float Seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        float MilleSeconds = timeToDisplay % 1 * 1000;
+
+        TimeText.text = string.Format("{0:0}:{1:00}:{2:00}", Minutes, Seconds, MilleSeconds);
+        finalScore.text = string.Format("{0:0}:{1:00}:{2:00}", Minutes, Seconds, MilleSeconds);
     }
 }
